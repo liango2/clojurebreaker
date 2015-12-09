451a2e6f4dc8c8ecca1fc039ccffb9da595b0e4d
liango2 <540625404@qq.com>
2015-12-09 13:35:32 +0800

    添加了一些说明文字

--- src/clojurebreaker/views/welcome.clj
+++ src/clojurebreaker/views/welcome.clj
@@ -10,16 +10,28 @@
     ))
 
 (defpartial board [{:keys [one two three four exact unordered]}]
-            [:div "1.-----------------------------------------"]
-            (when (and exact unordered)
-              [:div "完全匹配的个数: " exact " ,无序但能匹配的个数:" unordered])
-            [:div "2.-----------------------------------------"]
+            [:h1 "这是一个web版的Cojure电码破解游戏"]
+            [:p  "在这个游戏中, 程序创建了一个正序(N-orderd)的彩钉(r,g,b,y)密码.然后由人类玩家提交猜测的结果."]
+            [:ul "程序 对 玩家猜测的结果计分规则如下:"]
+            [:li "每当有一个颜色和位置都正确的彩钉, 就得到一枚黑色钉."]
+            [:li "每当有一个颜色正确但位置有误的彩钉, 就得到一枚白色钉."]
+            [:p  "全都猜对了, 或者是 猜错次数达到了上限, 游戏结束."]
+
+            [:line]
+            [:p "请输入(r,g,b,y):"]
             (form-to [:post "/guess"]
                      (text-field "one" one)
                      (text-field "two" two)
                      (text-field "three" three)
                      (text-field "four" four)
-                     (submit-button "Guess")))
+                     (submit-button "Guess"))
+
+            [:line]
+            [:p "得分:"]
+            (when (and exact unordered)
+              [:div "完全匹配的个数: " exact " ,无序但能匹配的个数:" unordered])
+
+            )
 
 (defpage "/" {:as guesses}
          (println "hello,world")
